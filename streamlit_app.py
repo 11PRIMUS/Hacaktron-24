2c645ed55d787412c9094de8dd6a03e30b802d82
Alok <112096255+11PRIMUS@users.noreply.github.com>
2024-10-19 11:22:48 +0530

    image upload feature

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,12 @@ brain_tumor_model = tf.keras.models.load_model(r'hackatron/brain_best.keras')
 # Title and instructions
 st.title('Cancer Classification (Breast, Brain Tumor)')
 st.write("Classify breast cancer, brain tumor using deep learning models.")
+uploaded_file = st.file_uploader("Choose an image for cancer classification...", type=["jpg", "png"])
+
+if uploaded_file is not None:
+    try:
+        image = Image.open(uploaded_file)
+        st.image(image, caption="Uploaded scan", use_column_width=True)
+        img = image.resize((224, 224))
+        img_array = np.array(img) / 255.0  # Normalize pixel values
+        img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
